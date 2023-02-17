745a5be4d8b84bf0fd9d4b95e086a85bfea92b50
Oded Shimoni <odedjshimoni@gmail.com>
2023-02-17 12:33:24 +0200

    2nd attempt

--- index.ts
+++ index.ts
@@ -1,37 +1,76 @@
-interface CleanConstructor {
-    new <T>(executor: (resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void, options: CleanOptions): CleanType<T>;
+interface IClean {
+    isRetryable: boolean;
+    isSuccess: () => boolean; // consider changing to a bool if needed, default is: success = promise resolved
+    // ... will be defined after testing some dynamic shit
 }
 
-interface CleanType<T> extends Promise<T> {
-    options: CleanOptions;
+class Clean<T> extends Promise<T> {
+    isRetryable: boolean;
+    isSuccess: Function;
+    revert: Function;
+    _resolvedValue: T;
+    then: any;
+    constructor(executor, options) {
+        const rawExecutor = executor;
+        const rescue = (onfulfilled, onrejected) => {
+                if (this.revert) {
+                const reverted = this.revert();
+                if (reverted) {
+                    onrejected('Reverted');
+                }
+            }
+        };
+
+        const wrapperThen = (onfulfilled, onrejected) => {
+            const originalOnRejected = onrejected;
+            onrejected = (e) => {
+                originalOnRejected(e);
+                rescue(onfulfilled, originalOnRejected);
+            }
+        };
+
+        super(executor);
+        this.then = wrapperThen;
+        const isBeforeAwaiting = !!options; // terrible name + not sure is correct nor best practice
+        const isAfterAwaiting = !options; // teririble name + not sure is correct nor best practice
+        if (isBeforeAwaiting) {
+            this.revert = options.revert;
+            this.isSuccess = options.isSuccess;
+        }
+    }
 }
 
-interface CleanOptions {
-    isRevertable?: boolean;
-}
+(async function() {
 
-function Clean<T>(executor, options: CleanOptions): CleanType<T> {
-    const promise = new Promise<T>(executor);
+    let variable = 0;
 
-    const clean = promise as CleanType<T>;
-    console.log('new clean arrived, world is about to change')
-    clean.options = options;
-    return clean;
-}
-Clean.prototype = Promise.prototype;
+    const addOne = new Clean(
+        (resolve, reject) => {
+            this.meta = this.meta || {};
+            this.meta.originalValue = variable;
 
-// class Clean extends Promise implements CleanType<any> {
-//     isRevertable: boolean;
-//     constructor(executor, { isRevertable = false }) {
-//         super(executor);
-//         this.isRevertable = isRevertable;
-//     }
-// }
+            const randBool = Math.random() > 0.5;
+            const toReject = randBool;
 
-const executor = function (resolve, _reject) {
-    resolve('lol');
-};
-const lol = Clean(executor, { isRevertable: true });
-lol.then((value) => {
-    console.log(value);
-});
+            ++variable;
+            if (toReject) {
+                reject('Couldn\'t do action');
+            } else {
+                resolve();
+            }
+        },
+        {
+            revert: () => {
+                variable = this.meta.originalValue;
+            },
+            isSuccess: () => true, // temp
+        }
+    );
+
+    try {
+        const res = await addOne;
+        console.log(variable);
+    } catch (e) {
+        console.log(variable);
+    }
+})();
