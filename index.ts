3ef5014edf74998cbc939420816c09338a92a1bc
Oded Shimoni <odedjshimoni@gmail.com>
2023-02-24 01:18:18 +0200

    Bootstrap - both 'then' and 'await' now working

--- index.ts
+++ index.ts
@@ -1,76 +1,96 @@
-interface IClean {
-    isRetryable: boolean;
-    isSuccess: () => boolean; // consider changing to a bool if needed, default is: success = promise resolved
-    // ... will be defined after testing some dynamic shit
-}
+// interface IClean {
+//     isRetryable: boolean;
+//     isSuccess: () => boolean;
 
-class Clean<T> extends Promise<T> {
-    isRetryable: boolean;
-    isSuccess: Function;
-    revert: Function;
-    _resolvedValue: T;
-    then: any;
-    constructor(executor, options) {
-        const rawExecutor = executor;
-        const rescue = (onfulfilled, onrejected) => {
-                if (this.revert) {
-                const reverted = this.revert();
-                if (reverted) {
-                    onrejected('Reverted');
-                }
-            }
-        };
+//     // consider changing to a bool if needed, default is: success = promise resolved
+//     // ... will be defined after testing some dynamic shit
+// }
 
-        const wrapperThen = (onfulfilled, onrejected) => {
-            const originalOnRejected = onrejected;
-            onrejected = (e) => {
-                originalOnRejected(e);
-                rescue(onfulfilled, originalOnRejected);
-            }
-        };
+// class Clean<TT> extends Promise<TT> {
+//     isRetryable: boolean;
+//     isSuccess: Function;
+//     revert: Function;
+//     rescue: Function;
+//     retryAttemptsCount: 0;
+//     maxRetryAttempts: -1;
+//     constructor(executor, options) {
+//         super(executor);
+//         // after 'super' call, internal promise function is already thened
+//         options && (this.isSuccess = options.isSuccess);
+//         options && (this.revert = options.revert);
+//         this.rescue = function() {
+//             const shouldRetry = this.isRetryable && this.retryAttemptsCount < this.maxRetryAttempts;
+//             if (shouldRetry) {
+//                 // try again somehow
+//                 return; // return something for short circuit
+//             }
+//             const shouldRevert = this.revert;
+//             if (shouldRevert) {
+//                 this.revert();
+//             }
+//         }
+//         this.then = function(onfulfilled, onrejected) {
+//             console.log('then wrapper');
+//             // im trying to find out where inner promise is being 'thened'
+//             return new Promise((resolve, reject) => {
+//                 // here the Clean resolves or rejects, regardless of the inner promise
+//                 const originalPromiseValue = onfulfilled('wtffff' as any);
+//                 if (!this.isSuccess(originalPromiseValue)) {
+//                     console.log('rescuing')
+//                     if (!this.rescue()) {
+//                         return reject(onrejected);
+//                     }
+//                 }
+//                 return resolve(originalPromiseValue);
+//             });
+//         }
+//     }
+// }
 
-        super(executor);
-        this.then = wrapperThen;
-        const isBeforeAwaiting = !!options; // terrible name + not sure is correct nor best practice
-        const isAfterAwaiting = !options; // teririble name + not sure is correct nor best practice
-        if (isBeforeAwaiting) {
-            this.revert = options.revert;
-            this.isSuccess = options.isSuccess;
-        }
-    }
-}
+// (async function() {
 
-(async function() {
+//     const originalValue = 0;
+//     let variable = 0;
 
-    let variable = 0;
+//     const addOne = new Clean(
+//         (resolve, reject) => {
+//             const toReject = true;
 
-    const addOne = new Clean(
-        (resolve, reject) => {
-            this.meta = this.meta || {};
-            this.meta.originalValue = variable;
+//             ++variable;
+//             if (toReject) {
+//                 reject('Couldn\'t do action');
+//             } else {
+//                 resolve('yay');
+//             }
+//         },
+//         {
+//             revert: () => {
+//                 variable = originalValue;
+//             },
+//             isSuccess: function() {
+//                 return variable === 1;
+//             },
+//         }
+//     );
 
-            const randBool = Math.random() > 0.5;
-            const toReject = randBool;
+//     // const anotherAsyncAction = new Clean((resolve, reject) => {
+//     //     resolve('lol');
+//     // },
+//     // {
+//     //     isSuccess: function(promiseResolvedValue) {
+//     //         return promiseResolvedValue === 'lol';
+//     //     }
+//     // });
 
-            ++variable;
-            if (toReject) {
-                reject('Couldn\'t do action');
-            } else {
-                resolve();
-            }
-        },
-        {
-            revert: () => {
-                variable = this.meta.originalValue;
-            },
-            isSuccess: () => true, // temp
-        }
-    );
 
-    try {
-        const res = await addOne;
-        console.log(variable);
-    } catch (e) {
-        console.log(variable);
-    }
-})();
+
+//     try {
+//         const res = await addOne;
+//         console.log(res);
+//         // const res2 = await anotherAsyncAction;
+//         // console.log(res2);
+//         // anotherAsyncAction.then(console.log);
+//     } catch (e) {
+//         console.log('Clean rejected');
+//     }
+// })();
